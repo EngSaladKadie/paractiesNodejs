a1d545790c62fc8585c3d62a69f4dca26f1fe01d
zalad <zaladyusuf@gmail.com>
2024-11-04 19:50:02 +0300

    eight commit

--- utils/fileHandller.js
+++ utils/fileHandller.js
@@ -1,3 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 const filePath = '/data/tasks.json';
+const writeTasksTofile =(tasks)=>{
+    fs.writeFileSync(filePath, JSON.stringify(tasks))
+
+}
+
+const readTaasksFromFile =()=>{
+    if(!fs.existsSync(filePath)){
+        writeTasksTofile([])
+    }
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
+}
