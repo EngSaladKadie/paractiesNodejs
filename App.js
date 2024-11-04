9b9d6ef3190e082677529cfc9f8ccf890ca6eb5e
zalad <zaladyusuf@gmail.com>
2024-11-04 18:40:54 +0300

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-const http = require('http');
-const { json } = require('stream/consumers');
-const PORT = 5000;
-const HOSTNMA = "localhost";
-const server= http.createServer((req, res) => {
-    if(req.startWith("/taskes")){
-        teskRoutes(req, res)
-    }
-    else{
-        res.writeHead(404,"NOT FOUND",{"Content-type":"application/json})"})
-        res.end(json({"message":"page not found"}))
-    }
-        
+const http= require('http');
+const PORT = 9000;
+const HOSTNAME= 'localhost';
+
+const server = http.createServer((req, res)=>{
+
 })
 
-server.listen(PORT, HOSTNMA, () => {            
-    console.log(`Server running at http://${HOSTNMA}:${PORT}`);
+
+server.listen(PORT,HOSTNAME, ()=>{
+    console.log(`server runing ${PORT}`);
+    
 })
