7c7d058566ddfa618462af84bcc932b0b2a74476
zalad <zaladyusuf@gmail.com>
2024-11-05 10:09:20 +0300

    first commit

--- utils/fileHandller.js
+++ utils/fileHandller.js
@@ -1,15 +1,21 @@
-const fs = require("fs");
-const path = require("path");
-const filePath = '/data/tasks.json';
-exports.writeTasksTofile =(tasks)=>{
-    fs.writeFileSync(filePath, JSON.stringify(tasks))
+const fs = require('fs');
+const path = require('path');
 
+const filePath = './data/tasks.json';
+
+exports.writeTasksToFile = (tasks) => {
+    if (Array.isArray(tasks) && tasks.length === 0) {
+        fs.writeFileSync(filePath, '[]');
+    } else {
+        fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+    }
 }
 
-exports.readTaasksFromFile =()=>{
-    if(!fs.existsSync(filePath)){
-        this.writeTasksTofile([]);
+exports.readTasksFromFile = () => {
+    if(!fs.existsSync(filePath)) {
+        this.writeTasksToFile([])
     }
+
     const data = fs.readFileSync(filePath);
-    return JSON.parse(data);
+    return JSON.parse(data)
 }
