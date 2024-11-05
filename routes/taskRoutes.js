b6a3075f49444c65cc8132f851b02bddcb7c251f
zalad <zaladyusuf@gmail.com>
2024-11-05 10:07:58 +0300

    first commit

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,18 +1,19 @@
-const { getTasks } = require("../controllers/teskController");
-const {createTask ,updateTask, deleteTask} = require('../controllers/teskController')
+const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControllers");
 
-const taskRoutes=(req,res)=>{
-    if(req.method === 'GET'){               
+const taskRoutes = (req, res) => {
+    if(req.method === 'GET') {
         getTasks(req, res);
-    }else if(req.method === 'POST'){
-        createTask(req, res);
-    }else if(req.method === 'PUT'){ 
-        updateTask(req, res);   
-    }else if(req.method === 'DELETE'){
-        deleteTask(req, res);   
-    }else{
-        res.writeHead(405,'data not found',{'Content-Type': 'application/json'});
-        res.end(JSON.stringify({error: 'page not allowed'}));
+    } else if(req.method === 'POST') {
+        createTask(req, res)
+    } else if (req.method === 'PATCH') {
+        updateTask(req, res)
+    } else if(req.method === 'DELETE') {
+        deleteTask(req, res)
+    } else {
+        res.writeHead(404, 'Data Not Found', { 'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: "Unknown Method required."
+        }))
     }
 }
 
