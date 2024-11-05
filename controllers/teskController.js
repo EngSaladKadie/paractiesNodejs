a899e0e4d05d03b661136574fa714fa0b509c299
zalad <zaladyusuf@gmail.com>
2024-11-05 10:03:19 +0300

    first commit

--- controllers/teskController.js
+++ controllers/teskController.js
@@ -60,3 +60,23 @@ exports.UpdateTask = async (req, res) => {
     }
 };
 
+exports.DeleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+      
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.status(200).json({
+            message: "Task deleted successfully",
+            data: deletedTask
+        });
+    } catch (error) {
+        console.error("Error deleting task:", error);
+        res.status(500).json({ message: "An error occurred while deleting the task" });
+    }
+};
